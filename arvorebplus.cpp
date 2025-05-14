f1d80bb2f73fb4df7769817c96f4dbde06f89468
Theosly <theoamagalhaespc@gmail.com>
2025-05-14 18:27:20 -0300

    search implemented

--- arvorebplus.cpp
+++ arvorebplus.cpp
@@ -1,4 +1,4 @@
-#include "arvorebplus.h"
+//#include "arvorebplus.h"
 #include <fstream>
 #include <vector>
 #include <iostream>
@@ -11,7 +11,7 @@ struct Node {
     bool isLeaf;
     int id;
     vector<int> chaves; // valor no nó 
-    vector<int> filhos; // IDs dos filhos 
+    vector<Node*> filhos; // IDs dos filhos 
     int n; // para sabermos quantas chaves há num nó
     string serialize() {
        //converter nó em um string para ser armazenado
@@ -47,11 +47,36 @@ class BTree {
             // se não, split e reajuste. 
   }
 }
+bool BuscarFolha(Node *x, int k){
+    Node* folha = BuscarNo(x, k);  
+    int i = 0; 
+    while(i < folha->n){
+        if (k == folha->chaves[i]) return true; 
+        i++;
+    }
+    return false; 
+}
 
-
+Node* BuscarNo(Node *x, int k){ // a busca é por ano da uva
+        if (x->isLeaf == true){
+            return x; 
+        }
+        int i = 0; 
+        while (i < x->n && k > x->chaves[i])
+            i++; 
+        if (k <= x->chaves[i]){
+            return BuscarNo(x->filhos[2*i+1], k);
+            // desce para o filho na posição (2*i + 1) do vetor de filhos (é o filho direito da chave na pos i)
+            // ou seja, estamos usando a convençao de que tudo à esquerda de uma chave é menor que a chave
+            // e à direita é maior ou igual à chave. 
+            // Temos que ir descendo nos filhos direitos até chegar na folha. 
+            // Depois de descermos uma vez, ou seja, usando o 2*i + 1, 
+            // chamamos a função recursivamente até batermos na folha
+            }
+        }
 void splitChild(int i, Node *y) {
    // hmm split..
    // tem que criar nós novos...
    // será que é melhor ter os nós como classe ou como struct?
 }
-}
+};
