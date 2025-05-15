1e3eb4de265b1b30197066bc8b2721ad584832f6
Theosly <theoamagalhaespc@gmail.com>
2025-05-15 17:45:46 -0300

    final implementations of the b+ methods

--- arvorebplus.cpp
+++ arvorebplus.cpp
@@ -190,7 +190,7 @@ class BTree {
     int raiz;
     int grau; // a arvore tem um certo grau.. 
     int nextId; // contador de ids
-    //BTree(int g) : raiz(0), grau(g), nextId(0){}
+    //BTree(int g) : raiz(NULL), grau(g), nextId(0){}
     // teste
     fstream& indexFile;
     string dataFile;
@@ -198,11 +198,11 @@ class BTree {
     bool indexDirty;
 
 
-    Node* criarNo(bool leaf) {
-        Node* no = new Node(leaf);
-        no->id = nextId++;
-        return no;
-    }
+    //Node* criarNo(bool leaf) {
+    //    Node* no = new Node(leaf);
+    //    no->id = nextId++;
+    //    return no;
+    //}
     int getAltura(){
         int height = 0;
         int currentId = raiz;
@@ -245,40 +245,50 @@ class BTree {
     void saveNode(Node& node) {
         node.serializeAndUpdate(buffer);
     }
-/*     void BTree::insert(int k) {
-        if (raiz->n == 0) { // se não há nenhuma chave na raiz, o n = 0
-            raiz->chaves.push_back(k); 
-            raiz->n = 1;
+    void insert(BTree* arvore, int k) {
+        int indice = buscar(arvore, k);
+        if (indice == -1){
+            int root_id = createNode(true);
+            arvore->raiz = root_id;  
+            Node root = loadNode(root_id);
+            root.chaves.push_back(k);
+            root.n = 1; 
+        }
+        Node noInserir = loadNode(indice);
+        if (noInserir.n == 2*arvore->grau - 1){
+            splitNode(arvore, noInserir);
+            insert(arvore, k);
         } 
-        else {
-            // busca onde inserir o registro
-            // vê se dá para inserir no nó específico
-            // se não, split e reajuste. 
+        else{
+            auto it = std::upper_bound(noInserir.chaves.begin(), noInserir.chaves.end(), k);
+            noInserir.chaves.insert(it, k);
+            noInserir.n++;
         }
-    } */
+    } 
 
-/*     bool BuscarFolha(BTree* arvore, int k, Node* buffer[2]){
-        buffer[1] = arvore->raiz; //pega a raiz e bota no buffer
-        Node* folha = BuscarNo(arvore->raiz, k, buffer);  //busca na raiz. Essa atribuição é permita porque, quando o nó
-        // folha for achado pela função BuscarNo, o nó terá sido posto no buffer, então, teremos ele na memória.
-        int i = 0; 
-        while(i < folha->n){
-            if (k == folha->chaves[i]) return true; 
-            i++;
+     int buscar(BTree* arvore, int k){
+        if (arvore->raiz == -1){
+            return -1; //arvore vazia
         }
-        return false; 
-    } */
-/* 
-    Node* BuscarNo(Node *x, int k, Node* buffer[2]){ // a busca é por ano da uva
-            if (x->isLeaf == true){
-                return x; 
+        Node raiz = loadNode(arvore->raiz);
+        int resultado = buscarNo(raiz, k);
+        // folha for achado pela função BuscarNo, o nó terá sido posto no buffer, então, teremos ele na memória.
+        return resultado;  
+    }
+
+    int buscarNo(Node x, int k){ // a busca é por ano da uva
+            if (x.isLeaf == true){
+                int i = 0;
+                while(i < x.n){
+                    if (k == x.chaves[i]) return x.id;
+                } 
             }
             int i = 0; 
-            while (i < x->n && k > x->chaves[i])
+            while (i < x.n && k > x.chaves[i])
                 i++; 
-            if (k <= x->chaves[i]){
-                buffer[1] = x->filhos[i]; 
-                return BuscarNo(x->filhos[i], k, buffer);
+            if (k <= x.chaves[i]){
+                Node proximo = loadNode(x.filhos[i]); 
+                return buscarNo(proximo, k);
                 // desce para o filho na posição (i) do vetor de filhos (é o filho esquerdo da chave na pos i
                 // e o filho direito da chave na pos i - 1)
                 // ou seja, estamos usando a convençao de que tudo à esquerda de uma chave é menor ou igual que a chave
@@ -287,32 +297,113 @@ class BTree {
                 // Depois de descermos uma vez, ou seja, usando o i, 
                 // chamamos a função recursivamente até batermos na folha
                 }
-            buffer[1] = x->filhos[(x->n)-1];
-            return BuscarNo(x->filhos[(x->n)-1], k, buffer); //caso que a chave é maior que todo mundo!
+            Node proximo = loadNode(x.filhos[i+1]); 
+            return buscarNo(proximo, k); //caso que a chave é maior que todo mundo!
             }
-    void splitLeaf(Node* folha, int indice) { //indice é a posição da qual acabamos de descer para chegarmos na folha,
+    void splitLeaf(BTree* arvore, Node folha) { //indice é a posição da qual acabamos de descer para chegarmos na folha,
         // ou seja, é a chave cujo filho esquerdo é essa folha. 
-        Node* novaFolha = new Node(true);
-        int total = folha->n + 1; 
+        int oldnodeId = folha.id; int newnodeId = createNode(true);
+        int temp = folha.proxFolha;
+        vector<int> temp_vec;
+        folha.proxFolha = newnodeId;
+        int total = folha.n + 1; 
         int meio = (total + 1)/2; // esse cálculo garante um meio melhor
-        novaFolha->chaves.assign(folha->chaves.begin() + meio, folha->chaves.end());
-        folha->chaves.resize(meio); // eu não queria usar isso... 
-        folha->n = folha->chaves.size(); // a quantidade de chaves é a quantidade de chaves que tem no vetor! wow!
-        novaFolha->n = novaFolha->chaves.size(); 
+        int temp_key = folha.chaves[meio]; 
+        int temp_pai = folha.pai; 
 
-        novaFolha->proxFolha = folha->proxFolha; // A -> C; B foi criado. B -> C, ou seja, aponta para quem A apontava
-        folha->proxFolha = novaFolha; // E, agora, A -> B, ou seja, A -> B -> C. 
-        if (folha->pai->n == 2*this->grau + 1){
-            splitNode(folha->pai);
+        temp_vec.assign(folha.chaves.begin() + meio, folha.chaves.end());
+        folha.chaves.resize(meio);  // eu não queria usar isso... 
+        folha.n = folha.chaves.size();
+        Node newnode = loadNode(newnodeId);
+        newnode.chaves.assign(temp_vec.begin() + meio, temp_vec.end());
+        newnode.n = newnode.chaves.size(); 
+        newnode.proxFolha = temp;  // A -> C; B foi criado. B -> C, ou seja, aponta para quem A apontava
+        // E, agora, A -> B, ou seja, A -> B -> C. 
+        newnode.pai = temp_pai; 
+        Node pai = loadNode(newnode.pai); 
+        if (pai.n == 2*arvore->grau - 1){
+            splitNode(arvore, pai);
+            insert(arvore, temp_key); 
         }
-        folha->pai->chaves.insert(folha->pai->chaves.begin() + indice, novaFolha->chaves[0]); // isso daqui eh só uma parada com iterator. 
-        folha->pai->filhos.insert(folha->pai->filhos.begin() + indice + 1, novaFolha); // fazer isso insere o valor sem substituir ninguém, dando um push no vetor
-        folha->pai->n++;
+        else{
+            auto it = std::upper_bound(pai.chaves.begin(), pai.chaves.end(), temp_key);
+            pai.chaves.insert(it, temp_key);
+            int i = 0; 
+            for(int filho : pai.filhos){
+                if (filho == oldnodeId){
+                    break;
+                }
+                i++; 
+            }
+            pai.filhos.insert(pai.filhos.begin() + i + 1, newnodeId); //adiciona o novo filho, deslocando
+            //todo mundo ao invés de substituir.   
+            pai.n++;
+        }
+    } 
+    void splitRoot(BTree* arvore, Node no){
+        int oldnodeId = no.id; int newnodeId; int temp;
+        if(no.isLeaf)  newnodeId = createNode(true); 
+        else newnodeId = createNode(false); 
+        if(no.isLeaf) int temp = no.proxFolha;
+        vector<int> temp_vec;
+        if(no.isLeaf) no.proxFolha = newnodeId;
+        int total = no.n + 1; 
+        int meio = (total + 1)/2; // esse cálculo garante um meio melhor
+        int temp_key = no.chaves[meio]; 
 
-    } */
-    void splitNode(Node* no) {
-    // hmm split..
-    // tem que criar nós novos...
-    // será que é melhor ter os nós como classe ou como struct?
+        temp_vec.assign(no.chaves.begin() + meio, no.chaves.end());
+        no.chaves.resize(meio);  // eu não queria usar isso... 
+        no.n = no.chaves.size();
+        Node newnode = loadNode(newnodeId);
+        newnode.chaves.assign(temp_vec.begin() + meio, temp_vec.end());
+        newnode.n = newnode.chaves.size(); 
+        if(no.isLeaf) newnode.proxFolha = temp;  // A -> C; B foi criado. B -> C, ou seja, aponta para quem A apontava
+        // E, agora, A -> B, ou seja, A -> B -> C. 
+        int newpaiId = createNode(false); 
+        Node pai = loadNode(newpaiId); 
+        pai.chaves[0] = temp_key; 
+        pai.filhos[0] = oldnodeId; 
+        pai.filhos[1] = newnodeId; 
+        pai.n++;
+    }
+    void splitNode(BTree* arvore, Node no){
+        int oldnodeId = no.id; int newnodeId = createNode(true);
+        vector<int> temp_vec;
+        int total = no.n + 1; 
+        int meio = (total + 1)/2; // esse cálculo garante um meio melhor
+        int temp_key = no.chaves[meio]; 
+        int temp_pai = no.pai; 
+
+        temp_vec.assign(no.chaves.begin() + meio, no.chaves.end());
+        no.chaves.resize(meio);  // eu não queria usar isso... 
+        no.n = no.chaves.size();
+        Node newnode = loadNode(newnodeId);
+        newnode.chaves.assign(temp_vec.begin() + meio, temp_vec.end());
+        newnode.n = newnode.chaves.size(); 
+        newnode.pai = temp_pai; 
+        Node pai = loadNode(newnode.pai); 
+        if (pai.n == 2*arvore->grau - 1){
+            splitNode(arvore, pai);
+            insert(arvore, temp_key); 
+        }
+        else{
+            auto it = std::upper_bound(pai.chaves.begin(), pai.chaves.end(), temp_key);
+            pai.chaves.insert(it, temp_key);
+            int i = 0; 
+            for(int filho : pai.filhos){
+                if (filho == oldnodeId){
+                    break;
+                }
+                i++; 
+            }
+            pai.filhos.insert(pai.filhos.begin() + i + 1, newnodeId); //adiciona o novo filho, deslocando
+            //todo mundo ao invés de substituir.   
+            pai.n++;
+        }
+    }
+    void splitNode(BTree* arvore, Node no) {
+        if (no.id == arvore->raiz) return (splitRoot(arvore, no)); 
+        else if (no.isLeaf) return splitLeaf(arvore, no); 
+        else return splitNode(arvore, no);
     }
 };
