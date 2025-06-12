ba1b3b08e8fb295d089ae60cf9c7216b911c767f
Theosly <theoamagalhaespc@gmail.com>
2025-06-12 18:17:45 -0300

    woof

--- trab_02/header/externalmerge.h
+++ trab_02/header/externalmerge.h
@@ -3,6 +3,6 @@
 
 #include <string>
 
-void external_merge_sort(const std::string& input_file, const std::string& output_file);
+void external_merge_sort(const std::string& input_file, int coluna, const std::string& output_file);
 
 #endif // EXTERNALMERGE_H
--- trab_02/src/externalmerge.cpp
+++ trab_02/src/externalmerge.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
 #include <vector>
 #include <algorithm>
 #include "../header/tupla.h"
@@ -11,17 +12,16 @@
 using namespace std;
 namespace fs = std::filesystem;
 
+//variaveis auxiliares
 const int TUPLAS_PAG = 10;
 const int MAX_PAG = 4; //é o B
 const int MAX_TUPLA = 10;
 const int MAX_BUFFER = MAX_PAG * MAX_TUPLA; // 40 tuplas
-int page_reads = 0;
-int page_writes = 0;
-
+int page_reads; int page_writes; 
 
 // Ordena e salva uma run no disco
-void sort_run(vector<Tupla>& run, int run_id) {
-    //aqui, o vetor de strings run é o vetor com as 40 tuplas que queremos ordenar no momento
+void sort_run(vector<Tupla>& run, int run_id, int coluna) {
+    //aqui, o vetor de strings run é o vetor com as tuplas que queremos ordenar no momento
     vector<Tupla> sorted = run;
     //explicando esse sort embaixo:
     /*
@@ -32,9 +32,9 @@ void sort_run(vector<Tupla>& run, int run_id) {
     return a < b;
     }
     */
-    sort(sorted.begin(), sorted.end(), [](const string& a, const string& b) {
-        int colA = stoi(a.substr(0, a.find(',')));
-        int colB = stoi(b.substr(0, b.find(',')));
+    sort(sorted.begin(), sorted.end(), [coluna](Tupla& a, Tupla& b) {
+        int colA = stoi(a.getColuna(coluna));
+        int colB = stoi(b.getColuna(coluna));
         return colA < colB; // pega a posição da tupla que corresponde ao índice primário e compara. 
     });
     int tuplas_total = 0;
@@ -74,10 +74,20 @@ vector<Tupla> read_buffer(Tabela* tabela, int start_page, int& next_page) {
     next_page = (i < tabela->qnt_pags) ? i : -1; //operador ternario... <3
     return buffer;
 }
+//poderia nao ser uma funcao, mas ia deixar o codigo do merge_runs feio :(
+vector<string> split_csv(const string& line) {
+    vector<string> cols;
+    stringstream ss(line);
+    string item;
+    while (getline(ss, item, ',')) {
+        cols.push_back(item);
+    }
+    return cols;
+}
 
 // Merge de múltiplos arquivos ordenados
-void merge_runs(const vector<string>& run_files, const string& output_file) {
-
+void merge_runs(const vector<string>& run_files, int coluna, const string& output_file) {
+    //a quantidade de páginas lidas nessa etapa será a quantidade de páginas escritas na etapa anterior
     vector<ifstream> inputs(run_files.size());
     vector<string> heads(run_files.size());
     vector<bool> active(run_files.size(), true);
@@ -91,7 +101,7 @@ void merge_runs(const vector<string>& run_files, const string& output_file) {
 
     ofstream out(output_file);
     vector<string> output_buffer;
-    int total_tuplas_escritas = 0;
+    int total_pags_escritas = 0;
 
     while(any_of(active.begin(), active.end(), [](bool a) { return a; })) {
         int min_val = INT64_MAX;
@@ -99,7 +109,8 @@ void merge_runs(const vector<string>& run_files, const string& output_file) {
 
         for (size_t i = 0; i < heads.size(); ++i) {
             if (active[i]) {
-                int chave = stoi(heads[i].substr(0, heads[i].find(',')));
+                vector<string> cols = split_csv(heads[i]); //dividir a linha em colunas 
+                int chave = stoi(cols[coluna]); //acessar o valor da coluna que queremos para ordenar baseando-se nessa coluna
                 if (min_idx == -1 || chave < min_val) {
                     min_val = chave;
                     min_idx = i;
@@ -114,33 +125,34 @@ void merge_runs(const vector<string>& run_files, const string& output_file) {
         if (output_buffer.size() == MAX_TUPLA) {
             for (const auto& linha : output_buffer) {
                 out << linha << "\n";
-                total_tuplas_escritas++;
-
             }
+            total_pags_escritas++;
             output_buffer.clear();
         }
-
+        //check se a próxima linha é não nula
         if (!getline(inputs[min_idx], heads[min_idx])) {
             active[min_idx] = false;
         }
     }
 
     // Escreve o restante
-    for (const auto& linha : output_buffer) {
+    if(!output_buffer.empty()){
+        total_pags_escritas++;
+        for (const auto& linha : output_buffer) {
         out << linha << "\n";
-        total_tuplas_escritas++;
     }
-
+    }
     for (auto& in : inputs) in.close();
     out.close();
-    int paginas_escritas = (total_tuplas_escritas + TUPLAS_PAG - 1) / TUPLAS_PAG;
-    page_writes += paginas_escritas;
+    page_writes += total_pags_escritas;
 }
 
 // Algoritmo de ordenação externa
-void external_merge_sort(Tabela* tabela, string& output_file) { 
+pair<int,int> external_merge_sort(Tabela* tabela, int coluna, string& output_file) { 
     int run_id = 0;
     int next_page = 0;
+    page_reads = 0;
+    page_writes = 0;
     vector<string> run_files;
     int run_id = 0;
     while (next_page != -1) {
@@ -148,21 +160,25 @@ void external_merge_sort(Tabela* tabela, string& output_file) {
 
         if (buffer.empty()) break;
 
-        sort_run(buffer, run_id);
+        sort_run(buffer, run_id, coluna);
         run_files.push_back("run_" + to_string(run_id) + ".txt");
         run_id++;
     }
     // ordena conjunto de runs!
+    int lidas;
     while (run_files.size() > 1) {
         vector<string> next_run_files;
         for (size_t i = 0; i < run_files.size(); i += 3) {
             //ordenamos de 3 em 3 e mantemos uma página de OUT
             vector<string> group;
+            lidas = 0;
             for (size_t j = i; j < i + 3 && j < run_files.size(); j++) {
+                lidas += (run_files[j].size()+ 9)/10; //qtd de paginas nessa run
                 group.push_back(run_files[j]);
             }
             string out_run = "run_tmp_" + to_string(i / 3) + ".txt";
-            merge_runs(group, out_run); ///ordenamos as páginas e as escrevemos no out run.
+            merge_runs(group, coluna, out_run); ///ordenamos as páginas e as escrevemos no out run.
+            page_reads += lidas; 
             next_run_files.push_back(out_run); 
         }
         //limpo o vetor de run_files
@@ -180,4 +196,6 @@ void external_merge_sort(Tabela* tabela, string& output_file) {
         */
     }
     if (!run_files.empty()) fs::rename(run_files[0], output_file); //Restará só uma run no vetor de runs.. que é o vetor todo! 
+    return make_pair(page_reads, page_writes);
+
 }
